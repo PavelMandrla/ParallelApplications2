5ae62639331ff3ccdf12bb5d109ee8097a6d987c
PavelMandrla <pavel.mandrla@gmail.com>
2021-02-24 20:19:57 +0100

    Initial commit of the cmake project in a runable state. The code is bad, but runs.

---
+++ src/runner.cu
@@ -0,0 +1,115 @@
+//
+// Created by pavel on 16.02.21.
+//
+
+#include <cudaDefs.h>
+#include <iostream>
+#include <benchmark.h>
+
+
+cudaError error = cudaSuccess;
+cudaDeviceProp deviceProp = cudaDeviceProp();
+
+constexpr unsigned int TPB = 256;    //threads per block
+constexpr unsigned int NOB = 16;    //number of blockss
+//constexpr unsigned int MBPTB = 2; //memory block per thread block -> posouvame se po vektoru dat -> kolikrat chci, at se posune
+
+using namespace std;
+
+__global__ void add1(const int* __restrict__ a, const int* __restrict__ b, const unsigned int length, int* __restrict__ c) { //__restrict__ nijak to nebude datove zasahovat do vektoru b - > nebuou mit spolecnou pamet
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+__global__ void fill(int* __restrict__ m, size_t pitch) {
+
+}
+
+int main() {
+    const int width = 10;
+    const int height = 10;
+
+    initializeCUDA(deviceProp);
+    //constexpr unsigned int length = 1 << 20;
+    constexpr unsigned int length = 1000;
+    const unsigned int sizeInBytes = length * sizeof(int);
+
+    //init host memory
+    int *dM = nullptr;
+    size_t pitch;
+    cudaMallocPitch((void**)&dM, &pitch, width * sizeof(int), height * sizeof(int));
+
+    dim3 dimBlock(width / 8, height / 8);
+    dim3 dimGrid(8, 8, 1); // velke mnozstvi bloku -> nevyhoda
+    fill<<<dimBlock, dimGrid>>>(dM, pitch);
+
+
+    //cudaMemcpy2D();
+
+
+    //int *a = (int*)malloc(sizeInBytes);   //the C way
+    //int *a = new int[length]; //volá interní konstruktory
+    int *a = static_cast<int*>(::operator new (sizeInBytes)); //C++ paralela k mallocu
+    int *b = static_cast<int*>(::operator new (sizeInBytes));
+    int *c = static_cast<int*>(::operator new (sizeInBytes));
+
+    //init data
+    for (int i = 0; i < length; i++) {
+        a[i] = 1;// rand();
+        b[i] = 2;//rand();
+        c[i] = 3;
+    }
+
+    //allocate device memory
+    int* da = nullptr;
+    int* db = nullptr;
+    int* dc = nullptr;
+    checkCudaErrors(cudaMalloc((void**)&da, sizeInBytes));
+    checkCudaErrors(cudaMalloc((void**)&db, sizeInBytes));
+    checkCudaErrors(cudaMalloc((void**)&dc, sizeInBytes));
+
+    //copy data from host to device
+    cudaMemcpy(da, a, sizeInBytes, cudaMemcpyKind::cudaMemcpyHostToDevice);
+    checkDeviceMatrix<int>(da, sizeInBytes, 1, length, "%d ", "Device A");
+
+    cudaMemcpy(db, b, sizeInBytes, cudaMemcpyKind::cudaMemcpyHostToDevice);
+    checkDeviceMatrix<int>(db, sizeInBytes, 1, length, "%d ", "Device B");  //helper metoda na výpis dat
+
+    cudaMemcpy(dc, c, sizeInBytes, cudaMemcpyKind::cudaMemcpyHostToDevice);
+    checkDeviceMatrix<int>(dc, sizeInBytes, 1, length, "%d ", "Device C");  //helper metoda na výpis dat
+
+    //Prepare grid and blocks
+    //dim3 dimBlock(TPB, 1, 1);
+    //dim3 dimGrid(NOB, 1, 1); // velke mnozstvi bloku -> nevyhoda
+
+    //dim3 dimGrid(getNumberOfParts(length, TPB), 1, 1); // velke mnozstvi bloku -> nevyhoda
+    //dim3 dimGrid(getNumberOfParts(length, TPB * MBPTB), 1, 1);
+
+    //Call kernel
+    using gpubenchmark::print_ns;
+    using gpubenchmark::print_time;
+
+    auto test = [&]() {add1<<<dimGrid, dimBlock>>>(da, db, length, dc);};   //obalim do lambda fce
+    print_time("Test ADD", test, 10);
+
+    cudaDeviceSynchronize();
+    checkDeviceMatrix<int>(dc, sizeInBytes, 1, length, "%d ", "Device C");  //helper metoda na výpis dat
+
+    //check results
+    cudaMemcpy(c, db, sizeInBytes, cudaMemcpyDeviceToHost);
+    checkHostMatrix(c, sizeInBytes, 1, length, "%d ", "Host C");
+    for (int i = 0; i < 20; i++) {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+
+
+    //free memory
+    delete[] a;
+    delete[] b;
+    delete[] c;
+
+    cudaFree(da); da = nullptr;
+    cudaFree(db); db = nullptr;
+    cudaFree(dc); dc = nullptr;
+}
